bb36bc54186e3bfaabf21628bf9aba12f731005f
Kriksman <76746851+kriksman@users.noreply.github.com>
2026-06-16 12:51:42 +0400

    Fix: Re-execute scripts from injected menu.html to enable burger toggle

--- shared/assets/load-menu.js
+++ shared/assets/load-menu.js
@@ -14,12 +14,30 @@
       return res.text();
     })
     .then(function (html) {
-      // Inject the HTML
       mount.innerHTML = html;
 
-      // The embedded script in menu.html will execute automatically
-      // Just ensure it's available
-      console.log('[menu-loader] Menu loaded successfully');
+      // Extract and re-execute scripts from the injected HTML
+      var scripts = mount.querySelectorAll('script');
+      scripts.forEach(function (oldScript) {
+        var newScript = document.createElement('script');
+        
+        // Copy all attributes
+        Array.prototype.forEach.call(oldScript.attributes, function (attr) {
+          newScript.setAttribute(attr.name, attr.value);
+        });
+
+        // Copy content or src
+        if (oldScript.src) {
+          newScript.src = oldScript.src;
+        } else {
+          newScript.textContent = oldScript.textContent;
+        }
+
+        // Replace and execute
+        oldScript.parentNode.replaceChild(newScript, oldScript);
+      });
+
+      console.log('[menu-loader] Menu loaded and scripts executed');
     })
     .catch(function (err) {
       console.error('[menu-loader]', err);
